ea8db5572fdaded1ffcfd3b930ab3e0e6a54721a
Juancruzlado <jcruzlado@gmail.com>
2024-08-03 04:28:42 -0300

    adding the README.md and modifying main-go

--- main.go
+++ main.go
@@ -1,26 +1,46 @@
 package main
 
 import (
+	"encoding/json"
 	"fmt"
 	"io"
 	"net/http"
 )
 
+type Feriado struct {
+	Fecha  string `json:"fecha"`
+	Nombre string `json:"nombre"`
+}
+
 func main() {
-	fmt.Println("Initial log!")
+	fmt.Println("[+] Initial log!\n")
+	fmt.Println("[+] Scanning info...\n")
+
 	res, err := http.Get("https://api.argentinadatos.com/v1/feriados/2024")
 	if err != nil {
 		panic(err)
 	}
 	defer res.Body.Close()
 
-	if res.StatusCode != 200 {
-		panic("Resource not avaliable")
+	if res.StatusCode != http.StatusOK {
+		panic("Resource not available")
 	}
 
 	body, err := io.ReadAll(res.Body)
 	if err != nil {
 		panic(err)
 	}
-	fmt.Println(string(body))
+
+	var feriados []Feriado
+	err = json.Unmarshal(body, &feriados)
+	if err != nil {
+		panic(err)
+	}
+
+	for _, feriado := range feriados {
+		fmt.Printf("Date: %s, Name: %s\n", feriado.Fecha, feriado.Nombre)
+	}
+
+	fmt.Println("\n")
+	fmt.Println("[+] Scan finished as expected, bye!\n")
 }
